241e5e208b9015b2c40bad5740dd7ae33115829f
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-01-07 16:52:30 -0600

    Knight validation

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 
 require 'pry-byebug'
 
@@ -22,11 +22,11 @@ class Game
     # Castling / Au Passant
     case piece.class.name
     when Pawn
-      # obstruction for forward move
+      # Still need to build en passant
       if piece.position[0] == target_x
         pawn_forward_move_valid?(piece, target_x, target_y)
       elsif (target_x - piece.position[0]).abs == 1 && (target_y - piece.position[1]).abs == 1
-        pawn_capture_move_valid?(piece, target_x, target_y)
+        capture_move_valid?(piece, target_x, target_y)
         # I think i can also use this for en passant as well, but would need to know about the last move of the piece next to it
         # Also need to check
       else
@@ -34,6 +34,8 @@ class Game
       end
 
 
+    when Knight
+      knight_move_valid?(piece, target_x, target_y)
     end
   end
 
@@ -49,7 +51,7 @@ class Game
     path_range.all? { |y| board[y][target_x].nil? }
   end
 
-  def pawn_capture_move_valid(piece, target_x, target_y)
+  def capture_move_valid(piece, target_x, target_y)
     if board[target_y][target_x]
       if piece.color == :white
         board[target_y][target_x].color == :black
@@ -59,6 +61,11 @@ class Game
     else
       false
     end
+  end
+
+  def knight_move_valid?(piece, target_x, target_y)
+    target_square = board[target_y][target_x]
+    target_square.nil? || target_square.color != piece.color
   end
 
 end

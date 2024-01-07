0511e6d9ddc3f78f7dc8bed889dd6027be573f84
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-01-07 10:07:16 -0600

    Continue on private methods for valid pawn moves

--- lib/game.rb
+++ lib/game.rb
@@ -12,6 +12,8 @@ class Game
   end
 
   def valid_move(piece,target_move)
+
+    (target_x,target_y) = target_move
     #checks to build:
     # Obstruction
     # Check
@@ -19,9 +21,28 @@ class Game
     # Castling / Au Passant
     case piece.class.name
     when Pawn
-      pass
+      #obstruction for forward move
+      if piece.position[0] == target_x
+        pawn_forward_move_valid?(piece, target_x, target_y)
+      elsif # Need to check if the target x and y deltas vs current position == 1
+        # Then implement the "pawn capture" check!
+        # I think i can also use this for en passant as well, but would need to know about the last move of the piece next to it
+        # Also need to check
+
+
     end
 
+  end
+
+  private
+
+  def pawn_forward_move_valid?(piece, target_x, target_y)
+    if piece.color == :white
+      path_range = (piece.position[1] + 1)...target_y
+    else
+      (target_y + 1)...piece.position[1]
+    end
+    path_range.all? { |y| board[y][target_x].nil? }
   end
 
 end

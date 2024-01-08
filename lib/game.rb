8fe3001140bdd4e60aecc70c6411663fbcce7f81
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-01-08 14:47:52 -0600

    Queen logic updates

--- lib/game.rb
+++ lib/game.rb
@@ -12,9 +12,8 @@ class Game
     @playing = true
   end
 
-  def valid_move(piece,target_move)
-
-    (target_x,target_y) = target_move
+  def valid_move(piece, target_move)
+    (target_x, target_y) = target_move
     #checks to build:
     # Obstruction
     # Check
@@ -22,20 +21,17 @@ class Game
     # Castling / Au Passant
     case piece.class.name
     when Pawn
-      # Still need to build en passant
-      if piece.position[0] == target_x
-        pawn_forward_move_valid?(piece, target_x, target_y)
-      elsif (target_x - piece.position[0]).abs == 1 && (target_y - piece.position[1]).abs == 1
-        capture_move_valid?(piece, target_x, target_y)
-        # I think i can also use this for en passant as well, but would need to know about the last move of the piece next to it
-        # Also need to check
-      else
-        false
-      end
-
-
+      pawn_move_valid?(piece,target_x, target_y)
     when Knight
       knight_move_valid?(piece, target_x, target_y)
+    when Bishop
+      bishop_move_valid?(piece, target_x, target_y)
+    when Rook
+      rook_move_valid?(piece, target_x, target_y)
+    when Queen
+      queen_move_valid?(piece, target_x, target_y)
+    when King
+      pass
     end
   end
 
@@ -51,7 +47,7 @@ class Game
     path_range.all? { |y| board[y][target_x].nil? }
   end
 
-  def capture_move_valid(piece, target_x, target_y)
+  def capture_move_valid?(piece, target_x, target_y)
     if board[target_y][target_x]
       if piece.color == :white
         board[target_y][target_x].color == :black
@@ -67,5 +63,74 @@ class Game
     target_square = board[target_y][target_x]
     target_square.nil? || target_square.color != piece.color
   end
+
+  def bishop_move_valid?(piece, target_x, target_y)
+    diagonal_path_clear?(piece, target_x, target_y) &&
+      (capture_move_valid?(piece, target_x, target_y) || board[target_y][target_x].nil?)
+  end
+
+  def pawn_move_valid?(piece, target_x, target_y)
+    # Still need to build en passant
+    if piece.position[0] == target_x
+      pawn_forward_move_valid?(piece, target_x, target_y)
+    elsif (target_x - piece.position[0]).abs == 1 && (target_y - piece.position[1]).abs == 1
+      capture_move_valid?(piece, target_x, target_y)
+      # I think i can also use this for en passant as well, but would need to know about the last move of the piece next to it
+      # Also need to check
+    else
+      false
+    end
+  end
+
+  def rook_move_valid?(piece, target_x, target_y)
+    #doesn't account for castling
+    straight_path_clear?(piece, target_x, target_y) &&
+      (capture_move_valid?(piece, target_x, target_y) || board[target_y][target_x].nil?)
+  end
+
+  def straight_path_clear?(piece, target_x, target_y)
+    path_range = []
+
+    if piece.position[0] = target_x
+      y_mod = target_y > piece.position[1] ? 1 : -1
+      steps = (target_y - piece.position[1]).abs
+      (1...steps).each do |index|
+        path_range << [piece.position[0], piece.position[1] + (y_mod * index)]
+      end
+
+    else
+      x_mod = target_x > piece.position[0] ? 1 : -1
+      steps = (target_x - piece.position[0]).abs
+      (1...steps).each do |index|
+        path_range << [piece.position[0] + (x_mod * index), piece.position[1]]
+      end
+    end
+
+    path_range.all? { |x, y| board[y][x].nil? }
+  end
+
+  def diagonal_path_clear?(piece, target_x, target_y)
+    x_mod = target_x > piece.position[0] ? 1 : -1
+    y_mod = target_y > piece.position[1] ? 1 : -1
+    path_range = []
+
+    steps = (target_x - piece.position[0]).abs
+    (1...steps).each do |index|
+      path_range << [piece.position[0] + (x_mod * index), piece.position[1] + (y_mod * index)]
+    end
+
+    path_range.all? { |x, y| board[y][x].nil? }
+  end
+
+  def queen_move_valid?(piece, target_x, target_y)
+    path_clear = if piece.position[x] == target_x || piece.position[y] == target_y
+                   straight_path_clear?(piece, target_x, target_y)
+                 else
+                   diagonal_path_clear?(piece, target_x, target_y)
+                 end
+    path_clear && (capture_move_valid?(piece, target_x, target_y) || board[target_y][target_x].nil?)
+  end
+
+
 
 end
